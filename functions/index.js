6335c9c3c3fd4fd1404d9b4da76025bcd3cc1cb5
kiyohken2000 <www.broadwell.com@gmail.com>
2021-03-31 10:40:10 +0900

    master

---
+++ functions/index.js
@@ -0,0 +1,50 @@
+const { Expo } = require('expo-server-sdk');
+const functions = require('firebase-functions');
+const admin = require('firebase-admin');
+
+// Expo のインスタンスを用意
+const expo = new Expo();
+
+// Firestore のインスタンスを用意
+admin.initializeApp();
+const settings = { timestampsInSnapshots: true };
+const db = admin.firestore();
+db.settings(settings);
+
+exports.sendNotification = functions.firestore
+  .document('talk/')
+  .onCreate(async () => {
+    // デバイストークンを Firestore から全検索
+    const query = db.collection('tokens').where('token', '>', '');
+    const snapshot = await query.get();
+    const tokens = snapshot.docs.map(doc => doc.get('token'));
+
+    // プッシュ通知用のメッセージオブジェクトを作成
+    const messages = [];
+    tokens.forEach(pushToken => {
+      if (!Expo.isExpoPushToken(pushToken)) {
+        console.error(`Push token ${pushToken} is not a valid Expo push token`);
+      }
+
+      messages.push({
+        to: pushToken,
+        sound: 'default',
+        body: 'This is a test notification',
+        data: { withSome: 'data' },
+      });
+    });
+    const chunks = expo.chunkPushNotifications(messages);
+    const tickets = [];
+    (async () => {
+      // Expo Push API をリクエスト
+      for (let chunk of chunks) {
+        try {
+          const ticketChunk = await expo.sendPushNotificationsAsync(chunk);
+          console.log(ticketChunk);
+          tickets.push(...ticketChunk);
+        } catch (error) {
+          console.error(error);
+        }
+      }
+    })();
+  });
