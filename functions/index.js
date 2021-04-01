40e79153f82c4f5346601fbb8dc0e430aff7a13d
kiyohken2000 <www.broadwell.com@gmail.com>
2021-04-01 10:51:58 +0900

    master

--- functions/index.js
+++ functions/index.js
@@ -14,22 +14,22 @@ exports.sendMessage = functions.region('asia-northeast2').firestore
     const talkName = newValue.name;
     const members = newValue.members;
     // console.log(name,text,members)
-    const messages = [];
 
     for (const elem of members) {
+      const message = [];
       const userRef = db.collection('tokens').doc(elem)
       userRef.get().then((doc) => {
         if (doc.exists) {
           const data = doc.data()
           const token = data.token
-            messages.push({
+            message.push({
               to: token,
               sound: 'default',
               title: talkName,
               body: text,
             });
-            console.log(messages)
-            expo.sendPushNotificationsAsync(messages)
+          console.log(message)
+          expo.sendPushNotificationsAsync(message)
         } else {
           null
         }
