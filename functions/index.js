339c0833e1888b788ccca661d342567325b03566
kiyohken2000 <www.broadwell.com@gmail.com>
2021-05-21 16:55:15 +0900

    master

--- functions/index.js
+++ functions/index.js
@@ -1,6 +1,7 @@
 const functions = require('firebase-functions');
 const admin = require('firebase-admin');
 const { Expo } = require('expo-server-sdk');
+const fetch = require('node-fetch');
 admin.initializeApp();
 
 const db = admin.firestore();
@@ -37,4 +38,25 @@ exports.sendMessage = functions.region('asia-northeast2').firestore
         } else { null }
       })
     }
+  });
+
+  exports.botMessage = functions.region('asia-northeast2').firestore
+    .document('THREADS/WIMi5WBba4N2XNtK5o5g/MESSAGES/{chatId}')
+    .onCreate((snap, context) => {
+      const newValue = snap.data();
+      const comment = newValue.text;
+      console.log(comment)
+
+      const formdata = {
+        'apikey':'DZZf7SoRWoozQmseljBkRKjvVKphwm8t',
+        'query':comment
+      }
+      fetch('https://api.a3rt.recruit-tech.co.jp/talk/v1/smalltalk',{
+        method: 'post',
+        body: formdata
+      }).then(response => {
+        response.json().then(data => {
+          console.log(data.results[0].reply);
+        })
+      }).catch(error => console.log(error));
   });
