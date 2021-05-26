1c0846c60903edb5d40d5774aa9ee8581b62a65a
kiyohken2000 <www.broadwell.com@gmail.com>
2021-05-26 14:07:23 +0900

    master

--- functions/index.js
+++ functions/index.js
@@ -2,6 +2,8 @@ const functions = require('firebase-functions');
 const admin = require('firebase-admin');
 const { Expo } = require('expo-server-sdk');
 const fetch = require('node-fetch');
+const vision = require('@google-cloud/vision');
+const axios = require("axios");
 admin.initializeApp();
 
 const db = admin.firestore();
@@ -45,34 +47,94 @@ exports.sendMessage = functions.region('asia-northeast2').firestore
     .onCreate((snap, context) => {
       const newValue = snap.data();
       const comment = newValue.text;
-      const u = newValue.user._id
+      const u = newValue.user._id;
 
-      if (u != 'XVY0p3KFxVaaQtq25JwlwWafUbs1') {
-        const params = new URLSearchParams();
-        params.append('apikey', "DZZf7SoRWoozQmseljBkRKjvVKphwm8t");
-        params.append('query', comment);
-        fetch('https://api.a3rt.recruit-tech.co.jp/talk/v1/smalltalk',{
-          method: 'post',
-          body: params
-        }).then(response => {
-          response.json().then(data => {
-            console.log(data.results[0].reply);
-            const text = data.results[0].reply;
-            const messageRef = db.collection('THREADS');
-            messageRef
-            .doc('WIMi5WBba4N2XNtK5o5g')
-            .collection('MESSAGES')
-            .add({
-              text,
-              createdAt: new Date().getTime(),
-              user: {
-                _id: 'XVY0p3KFxVaaQtq25JwlwWafUbs1',
-                email: 'pineprobot@pinepro.ml',
-                avatar: 'https://firebasestorage.googleapis.com/v0/b/kenmochat.appspot.com/o/avatar%2FXVY0p3KFxVaaQtq25JwlwWafUbs11621592754467?alt=media&token=f2366ddf-dc22-4977-a80c-496c5394c8fb',
-                name: 'PINE pro BOT',
+      if (comment) {
+        if (u != 'XVY0p3KFxVaaQtq25JwlwWafUbs1') {
+          const params = new URLSearchParams();
+          params.append('apikey', "DZZf7SoRWoozQmseljBkRKjvVKphwm8t");
+          params.append('query', comment);
+          fetch('https://api.a3rt.recruit-tech.co.jp/talk/v1/smalltalk',{
+            method: 'post',
+            body: params
+          }).then(response => {
+            response.json().then(data => {
+              console.log(data.results[0].reply);
+              const text = data.results[0].reply;
+              const messageRef = db.collection('THREADS');
+              messageRef
+              .doc('WIMi5WBba4N2XNtK5o5g')
+              .collection('MESSAGES')
+              .add({
+                text,
+                createdAt: new Date().getTime(),
+                user: {
+                  _id: 'XVY0p3KFxVaaQtq25JwlwWafUbs1',
+                  email: 'pineprobot@pinepro.ml',
+                  avatar: 'https://firebasestorage.googleapis.com/v0/b/kenmochat.appspot.com/o/avatar%2FXVY0p3KFxVaaQtq25JwlwWafUbs11621592754467?alt=media&token=f2366ddf-dc22-4977-a80c-496c5394c8fb',
+                  name: 'PINE pro BOT',
+                }
+              });
+            })
+          }).catch(error => console.log(error));
+        } else { null }
+      } else { null }
+  });
+
+  exports.imageBotMessage = functions.region('asia-northeast2').firestore
+    .document('THREADS/WIMi5WBba4N2XNtK5o5g/MESSAGES/{chatId}')
+    .onCreate((snap, context) => {
+      const newValue = snap.data();
+      const image = newValue.image;
+      const apiKey = 'AIzaSyAwHurWva6sJxhhIFiqTr3oqN5G_P4d0_8';
+      const visionApiUrl = `https://vision.googleapis.com/v1/images:annotate?key=${apiKey}`;
+      const options = {
+        requests: [
+          {
+            "features": [
+              {
+                "maxResults": 5,
+                "type": "LABEL_DETECTION"
               }
-            });
-          })
-        }).catch(error => console.log(error));
+            ],
+            "image": {
+              "source": {
+                "imageUri": image
+              }
+            }
+          }
+        ]        
+      };
+
+      if (image) {
+        (async function () {
+          try {
+            const result = await axios.post(visionApiUrl, options);
+            console.log("Request success!");
+            if (result.data) {
+              const labels = result.data.responses[0].labelAnnotations;
+              const descriptions = labels.map(label => label.description);
+              const dStr = descriptions.join('か');
+              const text = `これは${dStr} たぶんね`;
+              console.log(text)
+              const messageRef = db.collection('THREADS');
+              messageRef
+              .doc('WIMi5WBba4N2XNtK5o5g')
+              .collection('MESSAGES')
+              .add({
+                text,
+                createdAt: new Date().getTime(),
+                user: {
+                  _id: 'cYx7BY4HJWVL7KT7iAelCwiDaUl2',
+                  email: 'pinproimagebot@pinepro.ml',
+                  avatar: 'https://firebasestorage.googleapis.com/v0/b/kenmochat.appspot.com/o/avatar%2FcYx7BY4HJWVL7KT7iAelCwiDaUl21622003719314?alt=media&token=c4f520cb-4591-4670-b17d-9c96caaab08c',
+                  name: 'PINE pro image BOT',
+                }
+              });
+            }
+          } catch (error) {
+            console.error('error', error.response || error);
+          }
+        })();
       } else { null }
   });
